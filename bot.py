8d97bcd39bd03cee11623ee142547e3c39023cda
Ckflicks <109861478+Kichuprobotz@users.noreply.github.com>
2024-08-25 23:24:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logging.getLogger("imdbpy").setLevel(logging.ERROR)
 
 from pyrogram import Client, __version__
 from pyrogram.raw.all import layer
-from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_GROUP 
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN
 from typing import Union, Optional, AsyncGenerator
 from pyrogram import types
 from datetime import date, datetime 
